91825a03a7d2a84bf7bcfbd71d81bcc0fa27a45e
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-12-08 20:52:36 +0530

    Create Addition.java
    To adding the two numbers

---
+++ Addition.java
@@ -0,0 +1,13 @@
+// To declare the add two integer variables
+class Main{
+    public static void main(String args[]){
+        // declare one variable
+        int num1=23;
+        // decalre another variable 
+        int num2=45;
+        //  then adding the num1 and num2 variables
+        int add=num1+num2;
+        // printing the values
+        System.out.println("Addition of two numbers is:" +add);
+    }
+}
