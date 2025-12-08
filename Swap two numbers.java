eda1463695dda6781f2aef7a65571a0e7836f3a0
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-12-08 21:00:48 +0530

    Create Swap two numbers.java
    Swapping two numbers without using third number

---
+++ Swap two numbers.java
@@ -0,0 +1,14 @@
+// To  swap two variables without using a third variable 
+class Main{
+    public static void main(String args[]){
+        //declare a variable
+        int a=5;
+        // declare another variable
+        int b=7;
+        // then swapping the numbers.
+        a=a+b;
+        b=a-b;
+        System.out.println("a value is:" +a);//12
+        System.out.println("b value is:" +b);//5
+    }
+}
