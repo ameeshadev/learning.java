64ac5a947dafce8f6b04cf06d4fd2511a38a8c62
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-12-08 20:30:05 +0530

    Create Syntax.java

---
+++ Syntax.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi i am learning java");
+    }
+}
+// this is the syntax for the java programming language
+// public we use " it means the class can be accessed by anywhere "
+//class "means it is a blue print of a object and it deines how an object should look and behave
+"
