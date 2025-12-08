cd9f89f46cb23abc32771be0b656b4d3b590ca46
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-12-08 20:46:18 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,19 @@
+// variables of java programming language
+class Main{
+    public static void main (String args[]){
+        // there are string,int,char,bool, double etc..
+        //Now starts with String variable.
+        String name="My name is Ameesha";
+        System.out.println("name :" +name);
+        //Now start with integer variable
+        int num=3;
+        System.out.println("Number is:" +num);
+        // Now char variable
+        char letter='A';
+        System.out.println("character is:" +letter);
+        //Now Boolean variable.
+        boolean value=true;
+        System.out.println("boolean is" +value);
+
+    }
+}
