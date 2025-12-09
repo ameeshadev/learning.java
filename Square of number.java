ba8f1fdfe288284aa93fa279a90bc088d8896654
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-12-09 21:12:20 +0530

    Create Square of number.java

---
+++ Square of number.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        // writing a program on square of a number 
+        // we want number
+        int num=7;
+        int square=num*num;
+        System.out.println("Square=" +square);
+        //Square=49
+    }
+}
