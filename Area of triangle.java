14d64ca1dc6838b309b386d9252361cfab8a9fea
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-12-09 21:06:01 +0530

    Create Area of triangle.java

---
+++ Area of triangle.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        // writing a program on area of triangle
+        // we want length and width
+        int length=7;
+        int width=8;
+        int area=length*width;
+        System.out.println("area=" +area);
+        //area=56
+    }
+}
