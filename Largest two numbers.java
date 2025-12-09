780f5589ff71761976a263c1290e99d3a53b7e4a
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-12-09 21:15:58 +0530

    Create Largest two numbers.java

---
+++ Largest two numbers.java
@@ -0,0 +1,15 @@
+class  Main {
+    public static void main(String[] args) {
+        // writing a program on finding of largest of two numbers 
+        // we want  two numbers 
+        int a=10;
+        int b=20;
+        if(a<b){
+            system.out.println("a is large ");
+        }
+        else{
+        System.out.println("b is large");
+        }
+        // b is large
+       
+    }
