f51dc7645e3fb1623fd0fc99e16db43d8dbe6e52
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-12-09 21:09:56 +0530

    Create Simple intrest.java

---
+++ Simple intrest.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        // writing a program on simple intrest 
+        // we want length principle and time and rate of interest 
+        double p=1000;
+        double t=8;
+        double r=5;
+        double si=(p*t*r)/100;
+        System.out.println("Simple Intrest=" +si);
+      
+    }
+}
