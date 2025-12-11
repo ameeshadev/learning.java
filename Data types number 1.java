aba17df0ddf6ffe890ea0824304444e2203cb265
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-12-11 21:32:06 +0530

    Create Data types number 1.java

---
+++ Data types number 1.java
@@ -0,0 +1,14 @@
+class main{
+    public static void main (String args[]){
+        //Data Types
+        // first data type is.
+        int num=125679;
+        //the byte data type can store whole number from -2147483648 to 2147483647
+        System.out.println(num);
+        //second one is long data type.
+        short num1=60305478l;
+        System.out.println(num1);
+        // the short data type can store whole number from -9223372036854775808 to 9223372036854775807
+
+    }
+}
