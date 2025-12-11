193e3cba93a1834ed8018fe91068de5304ebea20
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-12-11 21:19:31 +0530

    Create Converting ASCII value.java

---
+++ Converting ASCII value.java
@@ -0,0 +1,11 @@
+class main{
+    public static void main (String args[]){
+        //checking the ASCII values.
+        //using char variable
+        char ch='D';
+        int ascii=ch;
+        System.out.println("ASCII value of D is:" +ascii);
+        //68
+
+    }
+}
