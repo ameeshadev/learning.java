da24890054df046712fb902a1fc0c57fa2ccf7b3
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-12-11 21:14:54 +0530

    Create Checking number.java

---
+++ Checking number.java
@@ -0,0 +1,18 @@
+class main{
+        public static void main(String args[]){
+            //checking positive or negative or zero.
+            int a=25;
+            if(a>0){
+                System.out.println("positive");
+            }
+            else if(a<0){
+                System.out.println("negative");
+                
+            }
+            else{
+                System.out.println("zero ");
+            }
+            //positive
+
+        }
+}
