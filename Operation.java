c6de1d19d7d0f2dea3b4259bd2db4a4d338fa17a
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-12-18 18:47:09 +0530

    Create Operation.java
    Arithmetic operations using datatypes

---
+++ Operation.java
@@ -0,0 +1,11 @@
+class Operations {
+    public static void main(String[] args) {
+        int a = 20, b = 10;
+
+        System.out.println("Sum = " + (a + b));
+        System.out.println("Difference = " + (a - b));
+        System.out.println("Product = " + (a * b));
+        System.out.println("Division = " + (a / b));
+    }
+}
+
