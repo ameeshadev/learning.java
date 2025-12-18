e6672fb0baee656ce0e5a9b7fa6ae3a8e4153571
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-12-18 18:43:04 +0530

    Create Primitive data types.java

---
+++ Primitive data types.java
@@ -0,0 +1,21 @@
+class PrimitiveTypes {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 25.5;
+        char c = 'A';
+        boolean d = true;
+        float e = 12.5f;
+        long f = 123456789L;
+        short g = 100;
+        byte h = 20;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
