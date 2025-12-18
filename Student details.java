28587392d59093753214a5286fbc42bbde835471
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-12-18 18:43:49 +0530

    Create Student details.java

---
+++ Student details.java
@@ -0,0 +1,13 @@
+class Student {
+    public static void main(String[] args) {
+        String name = "Ameesha";
+        int roll = 101;
+        double marks = 85.5;
+        char grade = 'A';
+
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + roll);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+    }
+}
