077ef2082ca7675f6b3ede5684804ba256a87fbd
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-12-18 18:46:12 +0530

    Create Average.java
    Using datatypes

---
+++ Average.java
@@ -0,0 +1,9 @@
+class Average {
+    public static void main(String[] args) {
+        double a = 10, b = 20, c = 30;
+        double avg = (a + b + c) / 3;
+
+        System.out.println("Average = " + avg);
+    }
+}
+
